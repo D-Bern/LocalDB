f52937ad776fb9a3a395ee9dbff0b73b579651d3
Daniel Bernier <dbernier@fundstream.com>
2022-05-16 13:24:44 -0400

    Add Colors table to migration

---
+++ AdventureWorksLT2019_LocalDB/Migrations/0.0.1-Baseline data/002_20220516-1322_dbernier.sql
@@ -0,0 +1,20 @@
+﻿-- <Migration ID="ae245f0d-1b7d-4eeb-beb7-3724261862bb" />
+GO
+
+PRINT N'Creating [dbo].[Colors]'
+GO
+CREATE TABLE [dbo].[Colors]
+(
+[ID] [int] NOT NULL IDENTITY(1, 1),
+[Name] [varchar] (128) NOT NULL,
+[HEX] [varchar] (8) NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_Colors] on [dbo].[Colors]'
+GO
+ALTER TABLE [dbo].[Colors] ADD CONSTRAINT [PK_Colors] PRIMARY KEY CLUSTERED ([ID])
+GO
+PRINT N'Creating extended properties'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Contains the supported colors.', 'SCHEMA', N'dbo', 'TABLE', N'Colors', NULL, NULL
+GO
---
+++ AdventureWorksLT2019_LocalDB/Migrations/0.0.1-Baseline data/003_20220516-1323_dbernier.sql
@@ -0,0 +1,24 @@
+﻿-- <Migration ID="bd452ff3-365a-4779-83d4-7515dfbbad55" />
+GO
+
+
+SET DATEFORMAT YMD;
+
+
+GO
+IF (SELECT COUNT(*)
+    FROM   [dbo].[Colors]) = 0
+    BEGIN
+        PRINT (N'Add 3 rows to [dbo].[Colors]');
+        SET IDENTITY_INSERT [dbo].[Colors] ON;
+        INSERT  INTO [dbo].[Colors] ([ID], [Name], [HEX])
+        VALUES                     (1, 'Blue', '0000FF');
+        INSERT  INTO [dbo].[Colors] ([ID], [Name], [HEX])
+        VALUES                     (2, 'Red', 'FF0000');
+        INSERT  INTO [dbo].[Colors] ([ID], [Name], [HEX])
+        VALUES                     (3, 'Green', '00FF00');
+        SET IDENTITY_INSERT [dbo].[Colors] OFF;
+    END
+
+
+GO
