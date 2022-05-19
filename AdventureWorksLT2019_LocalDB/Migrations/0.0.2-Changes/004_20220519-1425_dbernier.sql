89ac5d4dfdba77e56695c7027be07aa223bee643
Daniel Bernier <dbernier@fundstream.com>
2022-05-19 14:27:08 -0400

    Added migration script for Beer table

---
+++ AdventureWorksLT2019_LocalDB/Migrations/0.0.2-Changes/004_20220519-1425_dbernier.sql
@@ -0,0 +1,15 @@
+﻿-- <Migration ID="2163735f-9bca-40c3-b7ec-36f9d782c64f" />
+GO
+
+PRINT N'Creating [dbo].[Beer]'
+GO
+CREATE TABLE [dbo].[Beer]
+(
+[BeerID] [int] NOT NULL IDENTITY(1, 1),
+[Name] [varchar] (50) NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_Beer] on [dbo].[Beer]'
+GO
+ALTER TABLE [dbo].[Beer] ADD CONSTRAINT [PK_Beer] PRIMARY KEY CLUSTERED ([BeerID])
+GO
