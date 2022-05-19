4e70ad8a08d4598b21bf7831a67fd950e09152c1
Daniel Bernier <dbernier@fundstream.com>
2022-05-19 15:27:48 -0400

    Add Brewer migration script and rename some migration scripts

---
+++ AdventureWorksLT2019_LocalDB/Migrations/0.0.2-Changes/005_Add_table_Brewer.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="510f976f-af84-40a8-aa86-51111e67332d" />
+GO
+
+PRINT N'Creating [dbo].[Brewer]'
+GO
+CREATE TABLE [dbo].[Brewer]
+(
+[BrewerID] [int] NOT NULL IDENTITY(1, 1)
+)
+GO
+PRINT N'Creating primary key [PK_Brewer] on [dbo].[Brewer]'
+GO
+ALTER TABLE [dbo].[Brewer] ADD CONSTRAINT [PK_Brewer] PRIMARY KEY CLUSTERED ([BrewerID])
+GO
